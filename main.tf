9dfa252b2bb7d20929974bccfc7fb7631f7ebf55
edwardborges <eduardoluizb@yahoo.com.br>
2022-10-24 22:00:11 -0300

    commit inicial do terraform

--- main.tf
+++ main.tf
@@ -5,16 +5,18 @@ terraform {
       version = "~> 4.16"
     }
   }
-    required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0d593311db5abb72b"
   instance_type = "t2.micro"
+
+  key_name = "testeiac"
 
   tags = {
     Name = "Instancia Teste"
